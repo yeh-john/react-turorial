986701f214e205dbd994ee9564e022995171cbd7
john <john@localhost.localdomain>
2022-11-18 12:24:59 +0800

    done project v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import { useState, useRef } from "react";
 import TodoList from "./TodoList.jsx";
+import { v4 as uuidv4 } from 'uuid';
+import "./App.css";
 
 
 function App() {
-  const [todos, setTodos] = useState([
-    {id: 1, name: "Todo1", completed: false},
-  ]);
+  const [todos, setTodos] = useState([]);
 
 const todoNameRef = useRef();
 
 const handleAddTodo = () => {
   // Add task
-  console.log(todoNameRef.current.value)
+  const name = todoNameRef.current.value;
+  if(name === "") return;
+  setTodos((prevTodos) => {
+    return [...prevTodos, {id: uuidv4(), name: name, completed: false}]
+  });
+  todoNameRef.current.value = null;
+};
+
+const toggleTodo = (id) => {
+  const newTodos = [...todos];
+  const todo = newTodos.find((todo) => todo.id === id);
+  todo.completed = !todo.completed;
+  setTodos(newTodos);
+};
+
+const handleClear = () => {
+  const newTodos = todos.filter((todo) => !todo.completed);
+  setTodos(newTodos);
 };
 
   return(
     <div>
-      <TodoList todos={todos} />
+      <h1 class="h1">Todo list app</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={todoNameRef}/>
       <button onClick={handleAddTodo}>Add task</button>
-      <button>Done task</button>
-      <div>Task : 0</div>
+      <button onClick={handleClear}>Delete task</button>
+      <div>Task : {todos.filter((todo) => !todo.completed).length}</div>
     </div>
   );
 
